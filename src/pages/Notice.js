c339b031e36805689215284083d4b0f4d43190fb
fairwony <fairwony@gmail.com>
2024-09-05 17:53:34 +0900

    공지사항 메뉴 스타일 적용 중

--- src/pages/Notice.js
+++ src/pages/Notice.js
@@ -12,31 +12,31 @@ export default function Notice() {
           <ul className='notice-tab'>
             <li className='notice-on'>
               <div className='notice-img'>
-                <img src="images\Notice\cs_notice_on.svg" alt="확성기" />
+                <img src='images\Notice\cs_notice_on.svg' alt="확성기" />
               </div>
               <div className='notice-text'>공지사항</div>
             </li>
             <li>
               <div className='notice-img'>
-                <img src="images\Notice\cs_download.svg" alt="구름" />
+                <img src='images\Notice\cs_download.svg' alt="구름" />
               </div>
               <div className='notice-text'>자료실</div>
             </li>
             <li>
               <div className='notice-img'>
-                <img src="images\Notice\cs_faq.svg" alt="보고서" />
+                <img src='images\Notice\cs_faq.svg' alt="보고서" />
               </div>
               <div className='notice-text'>FAQ</div>
             </li>
             <li>
               <div className='notice-img'>
-                <img src="images\Notice\cs_as.svg" alt="스패너" />
+                <img src='images\Notice\cs_as.svg' alt="스패너" />
               </div>
               <div className='notice-text'>A/S 접수</div>
             </li>
             <li>
               <div className='notice-img'>
-                <img src="images\Notice\cs_review.svg" alt="말풍선" />
+                <img src='images\Notice\cs_review.svg' alt="말풍선" />
               </div>
               <div className='notice-text'>사용자 리뷰</div>
             </li>
