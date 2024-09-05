afe7294246a036d00fbe21740dd8c1cfcd9d4160
fairwony <fairwony@gmail.com>
2024-09-05 17:21:01 +0900

    공지사항 메뉴 개발 중

--- src/pages/Notice.js
+++ src/pages/Notice.js
@@ -8,37 +8,37 @@ export default function Notice() {
     <>
       <Header />
       <Main>
-        <div>
-          <ul>
-            <li>
-              <div className="notice-comm_title">
-                <img src="\images\Notice\cs_notice_on.svg" alt="확성기" />
+        <div className='notice-comm_title'>
+          <ul className='notice-tab'>
+            <li className='notice-on'>
+              <div className='notice-img'>
+                <img src="images\Notice\cs_notice_on.svg" alt="확성기" />
               </div>
-              <div>공지사항</div>
+              <div className='notice-text'>공지사항</div>
             </li>
             <li>
-              <div>
+              <div className='notice-img'>
                 <img src="images\Notice\cs_download.svg" alt="구름" />
               </div>
-              <div>자료실</div>
+              <div className='notice-text'>자료실</div>
             </li>
             <li>
-              <div>
+              <div className='notice-img'>
                 <img src="images\Notice\cs_faq.svg" alt="보고서" />
               </div>
-              <div>FAQ</div>
+              <div className='notice-text'>FAQ</div>
             </li>
             <li>
-              <div>
+              <div className='notice-img'>
                 <img src="images\Notice\cs_as.svg" alt="스패너" />
               </div>
-              <div>A/S 접수</div>
+              <div className='notice-text'>A/S 접수</div>
             </li>
             <li>
-              <div>
+              <div className='notice-img'>
                 <img src="images\Notice\cs_review.svg" alt="말풍선" />
               </div>
-              <div>사용자 리뷰</div>
+              <div className='notice-text'>사용자 리뷰</div>
             </li>
           </ul>
         </div>
