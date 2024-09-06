45467b43a499b5e4a7edf794981f238a9028150d
fairwony <fairwony@gmail.com>
2024-09-06 10:39:30 +0900

    메뉴 링크 개발중

--- src/pages/Notice.js
+++ src/pages/Notice.js
@@ -1,3 +1,4 @@
+import { Link } from 'react-router-dom';
 import Footer from '../components/Footer';
 import Header from '../components/Header';
 import Main from '../components/Main';
@@ -11,34 +12,35 @@ export default function Notice() {
         <div className="notice-comm_title">
           <ul className="notice-tab">
             <li className="notice-on">
+              <Link to=""
               <div className="notice-img">
                 <img src="images\Notice\cs_notice_on.svg" alt="확성기" />
               </div>
-              <div className='notice-text'>공지사항</div>
+              <div className="notice-txt">공지사항</div>
             </li>
-            <li>
-              <div className='notice-img'>
+            <li className="notice-on">
+              <div className="notice-img">
                 <img src="images\Notice\cs_download.svg" alt="구름" />
               </div>
-              <div className='notice-text'>자료실</div>
+              <div className="notice-txt">자료실</div>
             </li>
-            <li>
+            <li className="notice-on">
               <div className="notice-img">
                 <img src="images\Notice\cs_faq.svg" alt="보고서" />
               </div>
-              <div className="notice-text">FAQ</div>
+              <div className="notice-txt">FAQ</div>
             </li>
-            <li>
+            <li className="notice-on">
               <div className="notice-img">
                 <img src="images\Notice\cs_as.svg" alt="스패너" />
               </div>
-              <div className='notice-text'>A/S 접수</div>
+              <div className="notice-txt">A/S 접수</div>
             </li>
-            <li>
-              <div className='notice-img'>
-                <img src='images\Notice\cs_review.svg' alt="말풍선" />
+            <li className="notice-on">
+              <div className="notice-img">
+                <img src="images\Notice\cs_review.svg" alt="말풍선" />
               </div>
-              <div className='notice-text'>사용자 리뷰</div>
+              <div className="notice-txt">사용자 리뷰</div>
             </li>
           </ul>
         </div>
