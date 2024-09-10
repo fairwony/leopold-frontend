cfa484eb93da78f39b87b8b3abaa38dd28e8feaa
mosaek2 <mosaek2@gmail.com>
2024-09-10 18:46:21 +0900

    9/10

--- src/components/ShoppingDetailTable.js
+++ src/components/ShoppingDetailTable.js
@@ -1,17 +1,31 @@
+import { useState } from "react";
 import "./ShoppingDetailTable.css";
 
 export default function ShoppingDetailTable({ preview }) {
 
-	const printPreviewList = preview?.map((preview, index) => {
+	const [price, setPrice] = useState();
+	const [unit, setUnit] = useState();
+
+
+	const printPreviewList = preview?.map((pt, index) => {
+
+
 		return (
 			<div key={index}>
 				<p>
-					{preview.category2},{preview.category3},{preview.category4},{preview.category5},
+					{pt.category2},{pt.category3},{pt.category4},{pt.category5},
 					<input type="number" defaultValue={1} onChange={(e) => {
-						preview.quantity = parseInt(e.target.value);
-						console.log(preview);
+
+						setUnit(pt.price);
+
+						pt.quantity = parseInt(e.target.value);
+
+						pt.price = unit * parseInt(e.target.value);
+						setPrice(unit * parseInt(e.target.value));
+
+						console.log(pt);
 					}} />,
-					{preview.price}
+					{price}
 				</p>
 
 			</div>
