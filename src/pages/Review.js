860f44d2f6e161255c19a94e469fe1c195eb06a0
fairwony <fairwony@gmail.com>
2024-09-10 13:58:22 +0900

    review 제목 개발 중

--- src/pages/Review.js
+++ src/pages/Review.js
@@ -2,6 +2,7 @@ import { Link } from "react-router-dom";
 import Footer from "../components/Footer";
 import Main from "../components/Main";
 import WhiteHeader from "../components/WhiteHeader";
+import "./Review.css";
 
 export default function Review() {
 	return (
@@ -9,54 +10,59 @@ export default function Review() {
       <WhiteHeader />
       <Main>
         {/* 메뉴 바 */}
-        <div className="notice-comm_title">
-          <ul className="notice-tab">
-            <li className="notice-on">
+        <div className="review-comm_title">
+          <ul className="review-tab">
+            <li className="review-on">
               <Link to="/notice">
-                <div className="notice-img">
+                <div className="review-img">
                   <img src="\images\Download\cs_notice.svg" alt="확성기" />
                 </div>
                 <br />
-                <div className="notice-txt">공지사항</div>
+                <div className="review-txt">공지사항</div>
               </Link>
             </li>
-            <li className="notice-on">
+            <li className="review-on">
               <Link to="/download">
-                <div className="notice-img">
+                <div className="review-img">
                   <img src="\images\Notice\cs_download.svg" alt="구름" />
                 </div>
                 <br />
-                <div className="notice-txt">자료실</div>
+                <div className="review-txt">자료실</div>
               </Link>
             </li>
-            <li className="notice-on">
+            <li className="review-on">
               <Link to="/faq">
-                <div className="notice-img">
+                <div className="review-img">
                   <img src="\images\Notice\cs_faq.svg" alt="보고서" />
                 </div>
                 <br />
-                <div className="notice-txt">FAQ</div>
+                <div className="review-txt">FAQ</div>
               </Link>
             </li>
-            <li className="notice-on">
+            <li className="review-on">
               <Link to="/as">
-                <div className="notice-img">
+                <div className="review-img">
                   <img src="\images\Notice\cs_as.svg" alt="스패너" />
                 </div>
                 <br />
-                <div className="notice-txt">A/S 접수</div>
+                <div className="review-txt">A/S 접수</div>
               </Link>
             </li>
-            <li className="notice-on">
+            <li className="review-on">
               <Link to="/review">
-                <div className="notice-img">
+                <div className="review-img">
                   <img src="\images\Review\cs_review_on.svg" alt="말풍선" />
                 </div>
                 <br />
-                <div className="notice-txt">사용자 리뷰</div>
+                <div className="review-txt">사용자 리뷰</div>
               </Link>
             </li>
           </ul>
+        </div>
+        {/* 제목 */}
+        <div className="review-titleArea">
+          <h2>공지사항</h2>
+          <p>Notice</p>
         </div>
       </Main>
       <Footer />
