be97c1bc55796a506efb09f9dc1a96ee30fab0ec
fairwony <fairwony@gmail.com>
2024-09-11 16:14:12 +0900

    목록 버튼 개발 중

--- src/pages/NoticeDetail.js
+++ src/pages/NoticeDetail.js
@@ -65,85 +65,88 @@ export default function NoticeDetail() {
           <p>Notice</p>
         </div>
         {/* 게시판 */}
-        <div className="noticeDetail-board-read">
-          <div className="noticeDetail-board">
-            <div className="noticeDetail-base-table">
-              <table>
-                <tbody>
-                  <tr className="noticeDetail-bd_title">
-                    <td>
-                      <div className="noticeDetail-t01">
-                        {"프리미엄 알파셀 장패드 신규 출시"}
-                      </div>
-                      <div className="noticeDetail-t02">
-                        <span>{"Leopold"}</span>
-                        <span>{"2024-08-29"}</span>
-                      </div>
-                    </td>
-                  </tr>
-                  <tr className="noticeDetail-bd_content">
-                    <td>
-                      <div className="noticeDetail-fr-view">
-                        <p>
-                          <img
-                            src="\images\NoticeDetail\EC958CED8C8CEC858020EAB3B5ECA780.jpg"
-                            alt="알파셀 장패드"
-                            className="noticeDetail-fr-dib"
-                            style={{ width: "700px" }}
-                          />
-                        </p>
-                        <p>
-                          <br />
-                        </p>
-                        <p>
-                          <br />
-                        </p>
-                        <p>
-                          <span>
-                            <strong>{"🧡프리미엄 알파셀 장패드 출시🧡"}</strong>
-                          </span>
-                        </p>
-                        <p>
-                          <br />
-                        </p>
-                        <p>{"안녕하세요. 레오폴드입니다."}</p>
-                        <p>
-                          {
-                            "키보드 악세서리 프리미엄 알파셀 장패드가 새롭게 출시될 예정입니다."
-                          }
-                        </p>
-                        <p>
-                          {"자세한 내용은 제품 상세 페이지 참고 부탁드립니다!"}
-                        </p>
-                        <p>
-                          <br />
-                        </p>
-                        <p>
-                          <strong>{"[ 상세페이지 바로가기 ]"}</strong>
-                        </p>
-                        <p>
-                          <br />
-                        </p>
-                        <p>
+        <div className="noticeDetail-board">
+          <div className="noticeDetail-base-table">
+            <table>
+              <tbody>
+                <tr className="noticeDetail-bd_title">
+                  <td>
+                    <div className="noticeDetail-t01">
+                      {"프리미엄 알파셀 장패드 신규 출시"}
+                    </div>
+                    <div className="noticeDetail-t02">
+                      <span>{"Leopold"}</span>
+                      <span>{"2024-08-29"}</span>
+                    </div>
+                  </td>
+                </tr>
+                <tr className="noticeDetail-bd_content">
+                  <td>
+                    <div className="noticeDetail-fr-view">
+                      <p>
+                        <img
+                          src="\images\NoticeDetail\EC958CED8C8CEC858020EAB3B5ECA780.jpg"
+                          alt="알파셀 장패드"
+                          className="noticeDetail-fr-dib"
+                          style={{ width: "700px" }}
+                        />
+                      </p>
+                      <p>
+                        <br />
+                      </p>
+                      <p>
+                        <br />
+                      </p>
+                      <p>
+                        <span style={{ fontSize: "20px" }}>
+                          <strong>{"🧡프리미엄 알파셀 장패드 출시🧡"}</strong>
+                        </span>
+                      </p>
+                      <p>
+                        <br />
+                      </p>
+                      <p>{"안녕하세요. 레오폴드입니다."}</p>
+                      <p>
+                        {
+                          "키보드 악세서리 프리미엄 알파셀 장패드가 새롭게 출시될 예정입니다."
+                        }
+                      </p>
+                      <p>
+                        {"자세한 내용은 제품 상세 페이지 참고 부탁드립니다!"}
+                      </p>
+                      <p>
+                        <br />
+                      </p>
+                      <p style={{ color: "#000" }}>
+                        <strong>{"[ 상세페이지 바로가기 ]"}</strong>
+                      </p>
+                      <p>
+                        <br />
+                      </p>
+                      <p>
+                        <span style={{ fontSize: "22px" }}>
                           <strong>
-                            <span>
+                            <span style={{ color: "rgb(255, 108, 0)" }}>
                               {
                                 "판매일정 : 2024년 9월 2일 월요일 오전 11시부터 판매 시작"
                               }
                             </span>
                           </strong>
-                        </p>
-                        <p>
-                          <br />
-                        </p>
-                        <p>{"많은 관심 부탁드립니다."}</p>
-                        <p>{"감사합니다."}</p>
-                      </div>
-                    </td>
-                  </tr>
-                </tbody>
-              </table>
-            </div>
+                        </span>
+                      </p>
+                      <p>
+                        <br />
+                      </p>
+                      <p>{"많은 관심 부탁드립니다."}</p>
+                      <p>{"감사합니다."}</p>
+                    </div>
+                  </td>
+                </tr>
+              </tbody>
+            </table>
+          </div>
+          <div className="noticeDetail-base-button">
+            
           </div>
         </div>
       </Main>
