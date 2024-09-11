be9635bd4041a8c23376023f849b27687e53938e
fairwony <fairwony@gmail.com>
2024-09-11 18:35:33 +0900

    자료실 상세 페이지 개발 중

--- src/pages/DownloadDetail.js
+++ src/pages/DownloadDetail.js
@@ -1,9 +1,70 @@
 import "./DownloadDetail.css";
+import { Link } from "react-router-dom";
+import Main from "../components/Main";
+import WhiteHeader from "../components/WhiteHeader";
+import Footer from "../components/Footer";
 
 export default function DownloadDetail() {
   return (
     <>
-      
+      <WhiteHeader />
+      <Main>
+        <div id="downloadDetail-container">
+          {/* 메뉴 바 */}
+          <div className="downloadDetail-comm_title">
+            <ul className="downloadDetail-tab">
+              <li className="downloadDetail-on">
+                <Link to="/notice">
+                  <div className="downloadDetail-img">
+                    <img src="\images\Notice\cs_notice_on.svg" alt="확성기" />
+                  </div>
+                  <br />
+                  <div className="downloadDetail-txt">공지사항</div>
+                </Link>
+              </li>
+              <li className="downloadDetail-on">
+                <Link to="/download">
+                  <div className="downloadDetail-img">
+                    <img src="\images\Notice\cs_download.svg" alt="구름" />
+                  </div>
+                  <br />
+                  <div className="downloadDetail-txt">자료실</div>
+                </Link>
+              </li>
+              <li className="downloadDetail-on">
+                <Link to="/faq">
+                  <div className="downloadDetail-img">
+                    <img src="\images\Notice\cs_faq.svg" alt="보고서" />
+                  </div>
+                  <br />
+                  <div className="downloadDetail-txt">FAQ</div>
+                </Link>
+              </li>
+              <li className="downloadDetail-on">
+                <Link to="/as">
+                  <div className="downloadDetail-img">
+                    <img src="\images\Notice\cs_as.svg" alt="스패너" />
+                  </div>
+                  <br />
+                  <div className="downloadDetail-txt">A/S 접수</div>
+                </Link>
+              </li>
+              <li className="downloadDetail-on">
+                <Link to="/review">
+                  <div className="downloadDetail-img">
+                    <img src="\images\Notice\cs_review.svg" alt="말풍선" />
+                  </div>
+                  <br />
+                  <div className="downloadDetail-txt">사용자 리뷰</div>
+                </Link>
+              </li>
+            </ul>
+          </div>
+
+
+        </div>
+      </Main>
+      <Footer />
     </>
   );
 }
