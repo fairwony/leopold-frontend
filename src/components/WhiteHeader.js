992e7780fb14d9e847f675b6fd88b717d0dfeddb
mosaek2 <mosaek2@gmail.com>
2024-09-11 23:24:32 +0900

    header 재재수정

--- src/components/WhiteHeader.js
+++ src/components/WhiteHeader.js
@@ -10,23 +10,41 @@ export default function WhiteHeader() {
 			<div className="header-normal">
 				<div className="header-wrapper">
 					<div className="header-menu-wrapper">
-						<img src="\images\Header\h_menu_black.png" alt="menu_black" className="header-menu-icon"
-							onClick={() => { setIsMenuExtend(true); }} />
+						<img
+							src="\images\Header\h_menu_black.png"
+							alt="menu_black"
+							className="header-menu-icon"
+							onClick={() => {
+								setIsMenuExtend(true);
+							}}
+						/>
 					</div>
 
-					<Link to="/"><img src="\images\Header\logo_black.png" alt="logo_black" className="logo" /></Link>
+					<Link to="/">
+						<img
+							src="\images\Header\logo_black.png"
+							alt="logo_black"
+							className="logo"
+						/>
+					</Link>
 
 					<div className="header-container">
 						<p className="header-point">적립금 1,050P</p>
 						<p className="header-service">고객지원</p>
-						<img src="\images\Header\h_search_black.png" alt="search_black"></img>
-						<img src="\images\Header\h_mypage_black.png" alt="mypage_black"></img>
+						<img
+							src="\images\Header\h_search_black.png"
+							alt="search_black"
+						></img>
+						<img
+							src="\images\Header\h_mypage_black.png"
+							alt="mypage_black"
+						></img>
 						<img src="\images\Header\h_cart_black.png" alt="cart_black"></img>
 					</div>
 				</div>
 			</div>
 
-			
+
 
 			<div className={isMenuExtend ? "header-extend-yes" : "header-extend-no"}>
 				<img src="\images\Header\close.png" onClick={() => { setIsMenuExtend(false) }} alt="close"
