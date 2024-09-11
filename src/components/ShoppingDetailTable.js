01f4f6ccdf8f71a0c796683eee9f2f14d5521f1a
mosaek2 <mosaek2@gmail.com>
2024-09-11 13:47:09 +0900

    쇼핑상세보기 기능구현 완료

--- src/components/ShoppingDetailTable.js
+++ src/components/ShoppingDetailTable.js
@@ -1,34 +1,47 @@
-import { useState } from "react";
 import "./ShoppingDetailTable.css";
 
-export default function ShoppingDetailTable({ preview }) {
-
-	const [price, setPrice] = useState();
-	const [unit, setUnit] = useState();
+export default function ShoppingDetailTable({ preview, setPreview }) {
 
 
 	const printPreviewList = preview?.map((pt, index) => {
-
+		const fixedPrice = pt.price / pt.quantity;
 
 		return (
 			<div key={index}>
-				<p>
-					{pt.category2},{pt.category3},{pt.category4},{pt.category5},
-					<input type="number" defaultValue={1} onChange={(e) => {
+				<div style={{ display: "flex", alignItems: "center" }}>
+					<div style={{
+						display: "flex", width: 283, height: 49, justifyContent: "space-evenly", flexDirection: "column", gap: 7
+					}}>
+						<p className="sdt-font1">{pt.category2} {pt.category3}</p>
+						<p className="sdt-font2">- {pt.category4}/{pt.category5}</p>
+					</div>
 
-						setUnit(pt.price);
+					<input className="sdt-quantity-input" type="number" defaultValue={1} min={1} onChange={(e) => {
+						const newPreview = preview?.map((pt, idx) => {
+							if (idx === index) {
+								return {
+									...pt,
+									quantity: parseInt(e.target.value),
+									price: fixedPrice * parseInt(e.target.value)
+								}
+							}
+							return pt;
+						});
 
-						pt.quantity = parseInt(e.target.value);
-
-						pt.price = unit * parseInt(e.target.value);
-						setPrice(unit * parseInt(e.target.value));
+						setPreview(newPreview);
 
 						console.log(pt);
-					}} />,
-					{price}
-				</p>
+					}} />
 
-			</div>
+					<button className="sdt-cancel-btn" onClick={() => {
+						const newPreview = preview.filter((_, idx) => idx !== index);
+						setPreview(newPreview);
+					}}>X</button>
+
+					<p className="sdt-price-text">{pt.price?.toString()?.replace(/\B(?=(\d{3})+(?!\d))/g, ',')}원</p>
+				</div>
+
+			</div >
 		)
 	})
 
--- src/pages/ShoppingDetail.js
+++ src/pages/ShoppingDetail.js
@@ -8,8 +8,10 @@ import "./ShoppingDetail.css";
 export default function ShoppingDetail() {
 	const category2 = "FC900RBT MX2A";
 	const category3 = "코랄 블루";
-	const [category4, setCategory4] = useState();
-	const [category5, setCategory5] = useState();
+	const [category4, setCategory4] = useState("default");
+	const [category5, setCategory5] = useState("default");
+
+	const [preview, setPreview] = useState([]);
 
 
 
@@ -17,10 +19,11 @@ export default function ShoppingDetail() {
 	const printCategory4List = category4List?.map((list, index) => (
 		<option key={index} >{list}</option>
 	));
+
 	const category5List = ["갈축", "청축", "적축", "흑축", "저소음 적축", "백축", "은축"];
 	const printCategory5List = category5List?.map((list, index) => {
-		if (list === "저소음 적축" || list === "백축" || list === "은축"){
-			return(
+		if (list === "저소음 적축" || list === "백축" || list === "은축") {
+			return (
 				<option key={index} value={list}>{list} (+2,500원)</option>
 			)
 		}
@@ -34,13 +37,18 @@ export default function ShoppingDetail() {
 
 	function handleChangeCategory4(e) {
 		setCategory4(e.target.value);
-		setCategory5("default");
 	}
 
 	function handleChangeCategory5(e) {
 		setCategory5(e.target.value);
 
-		if (preview.some(obj => obj.category4 === category4, obj => obj.category5 === category5)) {
+		if (category4 === "default") {
+			alert("각인을 먼저 선택해 주세요.");
+			setCategory5("default");
+			return;
+		}
+
+		if (preview.some(obj => obj.category4 === category4 && obj.category5 === e.target.value)) {
 			alert("동일한 상품이 목록에 존재합니다.");
 			setCategory4("default");
 			setCategory5("default");
@@ -48,7 +56,9 @@ export default function ShoppingDetail() {
 		}
 
 		let price = 178000;
-		if (e.target.value === "흑축") price += 3500;
+		if (e.target.value === "저소음 적축" || e.target.value === "백축" || e.target.value === "은축") {
+			price += 2500;
+		}
 
 		const previewTable = {
 			category2: category2,
@@ -58,11 +68,11 @@ export default function ShoppingDetail() {
 			quantity: 1,
 			price: price
 		}
+
 		console.log(previewTable);
 		setPreview([...preview, previewTable]);
 	}
 
-	const [preview, setPreview] = useState([]);
 
 
 
@@ -99,21 +109,65 @@ export default function ShoppingDetail() {
 							</div>
 						</div>
 
-						<select value={category4} onChange={handleChangeCategory4}
-							onClick={() => { setCategory5("default") }}>
-							<option value="default">선택하세요</option>
-							{printCategory4List}
-						</select>
+						<div className="sd-category-box">
+							<p>각인</p>
 
-						<select value={category5} onChange={handleChangeCategory5}>
-							<option value="default">선택하세요</option>
-							{printCategory5List}
-						</select>
+							<select value={category4}
+								onChange={handleChangeCategory4}
+								onClick={() => { setCategory5("default") }}>
+								<option value="default">[필수] 옵션을 선택해 주세요</option>
+								{printCategory4List}
+							</select>
+						</div>
 
 
-						<ShoppingDetailTable preview={preview} />
-						<button onClick={() => { console.log(preview) }}>콘솔확인!</button>
+						<div className="sd-category-box">
+							<p>스위치</p>
+
+							<select value={category5}
+								onChange={handleChangeCategory5}>
+								<option value="default">[필수] 옵션을 선택해 주세요</option>
+								{printCategory5List}
+							</select>
+						</div>
+
+						<p className="sd-info-text1">
+							(최소주문수량 1개 이상)
+						</p>
+
+						<p className="sd-info-text2">
+							위 옵션선택 박스를 선택하시면 아래에 상품이 추가됩니다.
+						</p>
+
+						<ShoppingDetailTable preview={preview} setPreview={setPreview} />
+
+						<div style={{ display: "flex", alignItems: "baseline", marginTop: 30 }}>
+							<p style={{ fontSize: 13 }}>총 상품금액:&nbsp;</p>
+
+							<p style={{ fontSize: 20, fontWeight: "bold" }}>
+								{preview?.reduce((sum, pt) => sum + pt.price, 0)?.toString()
+									?.replace(/\B(?=(\d{3})+(?!\d))/g, ',')}원
+							</p>
+						</div>
+
+						<div style={{ display: "flex", gap: 17, marginTop: 32 }}>
+							<button className="sd-interest-btn">
+								<img src="\images\Shopping\interest.png" alt="interest" />
+							</button>
+
+							<button className="sd-cart-btn" onClick={() => { console.log(preview) }}>
+								장바구니
+							</button>
+
+							<button className="sd-buy-btn">
+								바로 구매
+							</button>
+						</div>
 					</div>
+				</div>
+
+				<div className="sd-detail-category-container">
+					
 				</div>
 			</Main>
 			<Footer />
