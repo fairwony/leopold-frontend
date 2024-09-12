b95f13ebec457fb42de4e64d996242b3b5a0478a
fairwony <fairwony@gmail.com>
2024-09-12 14:00:31 +0900

    일대일 문의게시판 개발중

--- src/pages/One2one.js
+++ src/pages/One2one.js
@@ -1,9 +1,16 @@
+import Footer from "../components/Footer";
+import WhiteHeader from "../components/WhiteHeader";
 import "./One2one.css";
 
 export default function One2one() {
   return (
     <>
-      
+      <WhiteHeader />
+      <Main>
+        {/* 제목 */}
+
+      </Main>
+      <Footer />
     </>
   );
 }
