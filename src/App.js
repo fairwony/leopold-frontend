4cfaee2d8bc77f05acb2503325004cd03722437e
mosaek2 <mosaek2@gmail.com>
2024-09-13 12:41:23 +0900

    start2

--- src/App.js
+++ src/App.js
@@ -27,42 +27,10 @@ import ShoppingDetail from "./pages/ShoppingDetail";
 import Support from "./pages/Support";
 import Test from "./pages/Test";
 import WriteReview from "./pages/WriteReview";
-import One2one from "./pages/One2one";
-import One2OneDetail from "./pages/One2OneDetail";
-import WriteOne2one from "./pages/WriteOne2one";
 
 export default function App() {
 	return (
 		<div className="App">
-			<Routes>
-				<Route path="/" element={<Home />} />
-				<Route path="/notice" element={<Notice />} />
-				<Route path="/notice/detail" element={<NoticeDetail />} />
-				<Route path="/download" element={<Download />} />
-				<Route path="/download/detail" element={<DownloadDetail />} />
-				<Route path="/notice" element={<Notice />} />
-				<Route path="/faq" element={<FAQ />} />
-				<Route path="/as" element={<AS />} />
-				<Route path="/asReception" element={<AsReception />} />
-				<Route path="/review" element={<Review />} />
-				<Route path="/review/notice" element={<ReviewNotice />} />
-				<Route path="/review/detail" element={<ReviewDetail />} />
-				<Route path="/login" element={<Login />} />
-				<Route path="/forgotId" element={<ForgotId />} />
-				<Route path="/forgotPs" element={<ForgotPs />} />
-				<Route path="/join" element={<Join />} />
-				<Route path="/mypage" element={<MyPage />} />
-				<Route path="/order" element={<Order />} />
-				<Route path="/order/detail" element={<OrderDetail />} />
-				<Route path="/shopping" element={<Shopping />} />
-				<Route path="/shopping/detail" element={<ShoppingDetail />} />
-				<Route path="/cart" element={<Cart />} />
-				<Route path="/support" element={<Support />} />
-				<Route path="/review/write" element={<WriteReview/>} />
-				<Route path="/one2one" element={<One2one />} />
-				<Route path="/one2one/detail" element={<One2OneDetail />} />
-				<Route path="/one2one/write" element={<WriteOne2one />} />
-			</Routes>
 			<MyContext>
 				<Routes>
 					<Route path="/" element={<Home />} />
