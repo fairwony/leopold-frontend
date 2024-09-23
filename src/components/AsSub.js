2991de9c63ea629095646d3cde35d010ea067d30
fairwony <fairwony@gmail.com>
2024-09-23 18:49:33 +0900

    as 아코디언

--- src/components/AsSub.js
+++ src/components/AsSub.js
@@ -6,7 +6,7 @@ export default function AsSub({ list }) {
 
   return (
     <>
-    {/* Q */}
+      {/* Q */}
       <div
         className="asSub-move"
         onClick={() => setIsOpen((isOpen) => !isOpen)}
@@ -15,7 +15,13 @@ export default function AsSub({ list }) {
           <img src="images\FAQ\faq_q.svg" alt="Q" />
         </span>
         {list?.question}
-        <svg width={24} height={24} viewBox="0 0 24 24" fill="none">
+        <svg
+          width={24}
+          height={24}
+          viewBox="0 0 24 24"
+          fill="none"
+          className={isOpen ? "asSub-arrow-up" : ""}
+        >
           <path
             d="M4 8L12 16L20 8"
             stroke="#1A1A1A"
@@ -25,17 +31,15 @@ export default function AsSub({ list }) {
           />
         </svg>
       </div>
-    {/* A */}
-      {isOpen && (
-        <div className="asSub-has-sub-a">
-          <div className="asSub-wrap">
-            <span>
-              <img src="\images\FAQ\faq_a.svg" alt="A"></img>
-            </span>
-            <div dangerouslySetInnerHTML={{ __html: list?.answer }} />
-          </div>
+      {/* A */}
+      <div className={isOpen ? "asSub-has-sub-a" : "asSub-has-sub-b"}>
+        <div className="asSub-wrap">
+          <span>
+            <img src="\images\FAQ\faq_a.svg" alt="A"></img>
+          </span>
+          <div dangerouslySetInnerHTML={{ __html: list?.answer }} />
         </div>
-      )}
+      </div>
     </>
   );
 }
